7fceb8617ae5b3ddd92d6849fef3542925b40482
Peempat Pinsang <peempat.pinsang@mail.kmutt.ac.th>
2021-02-10 12:46:41 +0700

    [UPLOAD]GROUPWORK3

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-    const app = {
-        data() {
-            return {
-                msg: 'Hello, Vue3'
-            }
+const app = {
+    data() {
+        return {
+            photos: [{url:'images/g1.jpg', title: 'Geysers valley, Russia',done: false},
+                     {url:'images/g2.jpg',title: 'Khumbu Valley, Nepal', done: false},
+                     {url:'images/g3.jpg',title: 'Waipio Valley , Hawaii', done: false}
+                ]
         }
-
+    },
+    methods: {
+        toggleDone(index){
+            this.photos[index].done = !this.photos[index].done
+        }
     }
-    Vue.createApp(app).mount('#app')
+    ,
+     computed: {
+        totalGallery(){
+            return this.photos.length
+        }
+    }
+}
+Vue.createApp(app).mount('#app')
